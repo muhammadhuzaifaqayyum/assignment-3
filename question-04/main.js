ed114d81771d4b331d66bd68b216c5c84654cc80
huzaifa <connectalhuzaifiii@gmail.com>
2023-03-28 03:06:29 +0500

    Update main.js

---
+++ question-04/main.js
@@ -0,0 +1,8 @@
+let name = "Muhammad Huzaifa Qayyum";
+let age = 21;
+
+if (age >= 18) {
+  console.log(`Welcome ${name}, you are an adult`);
+} else {
+  console.log(`Sorry ${name}, you are not yet an adult`);
+}
